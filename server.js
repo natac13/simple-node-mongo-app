72052478b7f5f1128d5a8ceba007eb05a81e9f3a
natac13 <sean.campbell13@gmail.com>
2016-02-17 10:07:39 -0500

    (setup):MongoDB
    Got the mongoDB connection working

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const indexRouter = require('./app/routes/index');
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
 
 const app = express();
 const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3300 ;
-app.use('/', indexRouter);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-})
+
+const mongoUrl = 'mongodb://localhost:27017/simple';
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err) {
+    console.log('Connection failed');
+    throw err;
+  } else {
+    console.log('Mongo Connection good on port 27017');
+  }
+
+  app.use(express.static(path.join(__dirname, './app/assets')));
+  app.use('/', indexRouter);
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+
+  });
+});
